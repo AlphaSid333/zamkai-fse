cdb1515acf4bd02bf1f676760eb07aa7320d9698
SidTGV <siddhi.sharma@themegrill.com>
2025-12-28 18:08:59 +0545

    Improvements on the templates and color improvements with new pattern too

---
+++ patterns/sidebar.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Title: Sidebar
+ * Slug: zamkai-fse/sidebar
+ * Categories: Uncategorized
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small","top":"var:preset|spacing|x-small"}}},"backgroundColor":"zk-alt-text","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-zk-alt-text-background-color has-background" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:search {"label":"Search","showLabel":false,"buttonText":"Search","buttonPosition":"button-inside","style":{"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}}}} /-->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":4} -->
+<h4 class="wp-block-heading">Categories</h4>
+<!-- /wp:heading -->
+
+<!-- wp:categories /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":4} -->
+<h4 class="wp-block-heading">Recent Posts</h4>
+<!-- /wp:heading -->
+
+<!-- wp:latest-posts {"displayPostDate":true,"columns":2,"style":{"spacing":{"padding":{"top":"0","bottom":"0"}}}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":4} -->
+<h4 class="wp-block-heading">Socials</h4>
+<!-- /wp:heading -->
+
+<!-- wp:social-links {"iconColor":"zk-primary","iconColorValue":"#e60000","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"left"}} -->
+<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"https://facebook.com","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"https://twitter.com","service":"x"} /-->
+
+<!-- wp:social-link {"url":"https://instagram.com","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"https://linkedin.com","service":"linkedin"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small"}},"border":{"radius":{"bottomLeft":"8px","bottomRight":"8px"}}},"backgroundColor":"zk-dark-background","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-zk-dark-background-background-color has-background" style="border-bottom-left-radius:8px;border-bottom-right-radius:8px;padding-top:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--x-small)"><!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|zk-alt-text"}}}},"textColor":"zk-alt-text"} -->
+<p class="has-text-align-center has-zk-alt-text-color has-text-color has-link-color">Make sure you...</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center","level":4,"style":{"elements":{"link":{"color":{"text":"var:preset|color|zk-alt-text"}}},"spacing":{"padding":{"top":"var:preset|spacing|tiny","bottom":"var:preset|spacing|tiny"}}},"textColor":"zk-alt-text","fontSize":"big"} -->
+<h4 class="wp-block-heading has-text-align-center has-zk-alt-text-color has-text-color has-link-color has-big-font-size" style="padding-top:var(--wp--preset--spacing--tiny);padding-bottom:var(--wp--preset--spacing--tiny)">Subscribe To <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-zk-primary-color">TechGrill</mark></h4>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
