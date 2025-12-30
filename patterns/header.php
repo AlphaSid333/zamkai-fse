4332ebdb69971ea039832685b7a1ca1edae21f3a
SidTGV <Siddhi.sharma@themegrill.com>
2025-12-30 22:37:39 +0545

    Responsive header

--- patterns/header.php
+++ patterns/header.php
@@ -6,20 +6,20 @@
  */
 ?>
 
-<!-- wp:group {"style":{"border":{"top":{"style":"none","width":"0px"},"right":{"style":"none","width":"0px"},"bottom":{"color":"#858585","width":"1px"},"left":{"style":"none","width":"0px"}}},"layout":{"type":"default"}} -->
-<div class="wp-block-group" style="border-top-style:none;border-top-width:0px;border-right-style:none;border-right-width:0px;border-bottom-color:#858585;border-bottom-width:1px;border-left-style:none;border-left-width:0px"><!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"padding":{"right":"var:preset|spacing|small","left":"var:preset|spacing|small","top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small"}}},"backgroundColor":"zk-dark-background"} -->
-<div class="wp-block-columns are-vertically-aligned-center has-zk-dark-background-background-color has-background" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|zk-alt-text"}}}},"textColor":"zk-alt-text"} -->
-<h2 class="wp-block-heading has-zk-alt-text-color has-text-color has-link-color"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-zk-primary-color">ZAMKAI</mark> FSE</h2>
+<!-- wp:group {"metadata":{"categories":["header"],"patternName":"zamkai-fse/header","name":"Header"},"style":{"border":{"top":{"style":"none","width":"0px"},"right":{"style":"none","width":"0px"},"bottom":{"color":"#858585","width":"1px"},"left":{"style":"none","width":"0px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="border-top-style:none;border-top-width:0px;border-right-style:none;border-right-width:0px;border-bottom-color:#858585;border-bottom-width:1px;border-left-style:none;border-left-width:0px"><!-- wp:columns {"verticalAlignment":"center","backgroundColor":"zk-dark-background"} -->
+<div class="wp-block-columns are-vertically-aligned-center has-zk-dark-background-background-color has-background"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|zk-alt-text"}}}},"textColor":"zk-alt-text"} -->
+<h2 class="wp-block-heading has-text-align-center has-zk-alt-text-color has-text-color has-link-color"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-zk-primary-color">ZAMKAI</mark> FSE</h2>
 <!-- /wp:heading --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:navigation {"ref":4,"textColor":"zk-alt-text","style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}},"layout":{"type":"flex","justifyContent":"center"}} /--></div>
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:navigation {"ref":4,"textColor":"zk-alt-text","overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}},"layout":{"type":"flex","justifyContent":"center"}} /--></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
 <div class="wp-block-buttons"><!-- wp:button -->
 <div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Contact Me</a></div>
 <!-- /wp:button --></div>
